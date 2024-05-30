7c909df3546e69c9a6e8567f219eb6a50875da12
Vennilavan <77039703+Vennilavan12@users.noreply.github.com>
2024-05-30 11:28:10 +0530

    Update prometheus.sh

--- prometheus-grafana-setup-master/prometheus.sh
+++ prometheus-grafana-setup-master/prometheus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROMETHEUS_VERSION="2.48.0"
+PROMETHEUS_VERSION="2.52.0"
 wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 tar -xzvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 cd prometheus-${PROMETHEUS_VERSION}.linux-amd64/
